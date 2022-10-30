70620da41a985fd6de0582242d759914596a51d5
iamfutureflash <xenode2000@gmail.com>
2022-10-30 23:12:11 +0530

    added responsive nevbar and slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      mdx: '860px',
+      mdxl: '880px',
+      lg: '1024px',
+      lgx: '1084px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
